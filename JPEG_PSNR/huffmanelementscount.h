d688a39e19c611aaca7af877287140b4c75c165d
AhmedH.Salamah <eng.ahmed_hussein@hotmail.com>
2019-10-02 17:30:18 -0400

    Second commit

---
+++ JPEG_PSNR/huffmanelementscount.h
@@ -0,0 +1,23 @@
+//
+//  huffmanelementscount.hpp
+//  TCM
+//
+//  Created by Hossam Amer on 2018-08-09.
+//  Author: Hossam Amer & Yanbing Jiang, University of Waterloo
+//  Copyright © 2018 All rights reserved.
+//
+
+#ifndef HUFFMANELEMENTSCOUNT_H
+#define HUFFMANELEMENTSCOUNT_H
+
+using namespace std;
+#include <vector>
+class HuffmanElementsCount
+{
+public:
+    int codeLength;
+    vector<int>elementsCodedWithCodeLengthBits;
+    HuffmanElementsCount();
+};
+
+#endif // HUFFMANELEMENTSCOUNT_H
---
+++ JPEG_PSNR/huffmantable.h
@@ -0,0 +1,42 @@
+//
+//  huffmantable.hpp
+//  TCM
+//
+//  Created by Hossam Amer on 2018-08-09.
+//  Author: Hossam Amer & Yanbing Jiang, University of Waterloo
+//  Copyright © 2018 All rights reserved.
+//
+
+#ifndef HUFFMANTABLE_H
+#define HUFFMANTABLE_H
+
+#include <map>
+#include <vector>
+#include "TypeDef.h"
+
+using namespace std;
+
+class HuffmanTable {
+    
+public:
+    
+    // ID for which Huffman table it is using (there are 32 possible Huffman tables in JPEG)
+    unsigned char tableID;
+    
+    // table length from bitstream
+    unsigned short tableSegmentLengthFromBitstream;
+    
+    // tableClass 0 is for DC, 1 is for AC
+    unsigned char  tableClass;
+    vector <unsigned int> codes;
+    vector <unsigned int> codeLengths;
+    
+    vector <unsigned char> number_of_codes_for_each_1to16;
+    
+    // The array of Huffman maps: (length, code) -> value
+    std::map<huffKey, unsigned char> huffData;
+    
+    HuffmanTable();
+};
+
+#endif // HUFFMANTABLE_H
